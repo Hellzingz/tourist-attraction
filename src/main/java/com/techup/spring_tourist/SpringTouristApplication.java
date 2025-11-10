a027ebcb9cc357fd36b30d1f39b2f0498d1518a4
Supakit Jaratworapat <supakit.jaratworapat@gmail.com>
2025-11-10 11:15:23 +0700

    Initialize Spring Boot project with Maven wrapper and configuration files. Added .gitignore, .gitattributes, and pom.xml for project dependencies. Created main application class and test class, along with application properties for configuration.

---
+++ src/main/java/com/techup/spring_tourist/SpringTouristApplication.java
@@ -0,0 +1,13 @@
+package com.techup.spring_tourist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTouristApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTouristApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techup/spring_tourist/SpringTouristApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techup.spring_tourist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTouristApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
